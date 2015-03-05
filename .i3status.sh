cfe6e2559aae63d8d5092c533fbaeb0840361e95
Rafael Fernández López <ereslibre@ereslibre.es>
2015-03-05 17:55:38 +0100

    Add my i3 config

---
+++ .i3status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+i3status --config ~/.i3status.conf | while :
+do
+    read line
+    layout=$(setxkbmap -query | awk '/layout/{print $2}')
+    head="[{ \"full_text\": \"LANG: $layout\", \"color\": \"#FFFFFF\" },"
+    echo "${line/[/$head}" || exit 1
+done
