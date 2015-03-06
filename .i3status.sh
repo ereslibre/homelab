1e8437c55e32b90c5923c34775bc1d057052a52b
Rafael Fernández López <ereslibre@ereslibre.es>
2015-03-06 01:59:28 +0100

    Add spacing

--- .i3status.sh
+++ .i3status.sh
@@ -3,6 +3,6 @@ i3status --config ~/.i3status.conf | while :
 do
     read line
     layout=$(setxkbmap -query | awk '/layout/{print $2}')
-    head="[{ \"full_text\": \"$layout\", \"color\": \"#FFFFFF\" },"
+    head="[{ \"full_text\": \" $layout \", \"color\": \"#FFFFFF\" },"
     echo "${line/[/$head}" || exit 1
 done
