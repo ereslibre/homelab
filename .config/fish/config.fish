2196e0d83b2249d6b892afe46600083aa832b03e
Rafael Fernández López <ereslibre@ereslibre.es>
2015-08-14 01:27:38 +0200

    Use operators without spaces

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
 set fish_greeting
 
 if [ -x /usr/local/bin/keychain ]
-    keychain --nogui
+    keychain --nogui ^/dev/null
     . $HOME/.keychain/$HOSTNAME-fish
 end
 
-if boot2docker status | grep running > /dev/null
-  set shinit (boot2docker shellinit ^ /dev/null)
+if boot2docker status | grep running >/dev/null
+  set shinit (boot2docker shellinit ^/dev/null)
   for i in (seq (count $shinit))
     eval $shinit[$i]
   end
