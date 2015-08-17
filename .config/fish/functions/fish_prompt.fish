9e0491b4e74e99bd8580faf1174765223cbcd36d
Rafael Fernández López <ereslibre@ereslibre.es>
2015-08-17 17:09:33 +0200

    Use $USER to paint current user

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -90,7 +90,7 @@ function fish_prompt --description 'Write out the prompt'
         if [ $USER = "ereslibre" ]; and [ $__fish_prompt_hostname = "" ]
           set __fish_prompt_user_and_hostname
         else
-          set __fish_prompt_user_and_hostname (set_color $fish_color_user) "$__fish_prompt_user" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '
+          set __fish_prompt_user_and_hostname (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '
         end
 
 	echo -n -s $__fish_prompt_user_and_hostname (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" " > "
