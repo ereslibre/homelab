21f421ddf86b0dbfe3ec91a6e2f5a21bd837d72b
Rafael Fernández López <ereslibre@ereslibre.es>
2015-09-21 13:22:02 +0200

    Change defaults

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 
-set -x EDITOR "emacs -nw --no-desktop"
-set -x GIT_EDITOR "emacs -nw --no-desktop"
+set -x EDITOR "emacs -nw -q"
+set -x GIT_EDITOR "emacs -nw -q"
 
 if [ -x /usr/local/bin/keychain ]
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
