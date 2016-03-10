023518a3718fb221f52ef5b2783e4bd891b8613b
Rafael Fernández López <ereslibre@ereslibre.es>
2016-03-10 19:07:37 +0100

    Update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -87,10 +87,14 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-        if [ $USER = "ereslibre" ]; and [ $__fish_prompt_hostname = "" ]
+        if begin; [ $USER = "ereslibre" ]; and [ $__fish_prompt_hostname = "Rafaels-MacBook-Pro" ]; end
           set __fish_prompt_user_and_hostname
         else
-          set __fish_prompt_user_and_hostname (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '
+          if [ $USER = "ereslibre" ]
+            set __fish_prompt_user_and_hostname (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '
+          else
+            set __fish_prompt_user_and_hostname (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' '
+          end
         end
 
 	echo -n -s $__fish_prompt_user_and_hostname (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" " > "
