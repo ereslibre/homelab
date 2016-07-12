edfc6e76f3e80e1e9ce46b25f8e433b2f97a3cfe
Rafael Fernández López <ereslibre@ereslibre.es>
2016-07-13 01:58:33 +0200

    Cleanup

--- .i3status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i3status --config ~/.i3status.conf | while :
-do
-    read line
-    layout=$(setxkbmap -query | awk '/layout/{print $2}')
-    head="[{ \"full_text\": \" $layout \", \"color\": \"#FFFFFF\" },"
-    echo "${line/[/$head}" || exit 1
-done
--- .toggle-keyboard-layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-layout=$(setxkbmap -query | awk '/layout/{print $2}')
-if [ $layout == 'us' ]
-then
-    setxkbmap es
-else
-    setxkbmap us
-fi
-killall -SIGUSR1 i3status
