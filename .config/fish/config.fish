17911c9e7d900aded85dab8ad3cc2e36c745cf7f
Rafael Fernández López <ereslibre@ereslibre.es>
2016-10-29 12:03:45 +0200

    Add some configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,13 @@ cat ~/.todo
 set -x EDITOR "emacs -nw -q"
 set -x GIT_EDITOR "emacs -nw -q"
 
-set PATH $HOME/bin $PATH
+set -x CC "ccache gcc"
+set -x CXX "ccache g++"
+
+set PATH $HOME/bin /usr/lib/ccache/bin $PATH
+
+set -x KUBERNETES_PROVIDER "vagrant"
+set -x VAGRANT_DEFAULT_PROVIDER "virtualbox"
 
 if [ -x /usr/bin/keychain ]
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
