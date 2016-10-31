f26792dd7ff1e55e3be637be925cca70eff48837
Rafael Fernández López <ereslibre@ereslibre.es>
2016-11-01 00:48:45 +0100

    Let's go forward to make everything run in containers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,8 @@ cat ~/.todo
 set -x EDITOR "emacs -nw -q"
 set -x GIT_EDITOR "emacs -nw -q"
 
-set -x CC "ccache gcc"
-set -x CXX "ccache g++"
-
 set -x GOPATH $HOME/projects/go-workspace
-set PATH $GOPATH/bin $HOME/bin /usr/lib/ccache/bin $HOME/projects/kubernetes/kubernetes/third_party/etcd $PATH
+set PATH $GOPATH/bin $HOME/bin /usr/lib/ccache/bin $PATH
 
 set -x KUBERNETES_PROVIDER "vagrant"
 set -x VAGRANT_DEFAULT_PROVIDER "virtualbox"
