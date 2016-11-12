0fb209e3eb2b04286c61817b413216a81c6462a5
Rafael Fernández López <ereslibre@ereslibre.es>
2016-11-12 16:35:13 +0100

    Alias docker compose

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ set -x GIT_EDITOR "emacs -nw -q"
 
 set PATH $HOME/.bin $PATH
 
+alias dc docker-compose
+
 if [ -x /usr/bin/keychain ]
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
     source $HOME/.keychain/(hostname)-fish
