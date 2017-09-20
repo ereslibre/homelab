90a6b525b77224c2554eb79ea8deb2d9f23e5915
Rafael Fernández López <ereslibre@ereslibre.es>
2017-09-20 07:48:45 +0200

    Add some useful webjumps

--- .conkerorrc/000-init.js
+++ .conkerorrc/000-init.js
@@ -2,5 +2,9 @@ url_completion_use_history = false;
 url_completion_use_bookmarks = true;
 can_kill_last_buffer = true;
 
+define_webjump("g", "http://www.google.com/search?q=%s");
+define_webjump("w", "https://en.wikipedia.org/w/index.php?search=%s");
+define_webjump("ws", "https://es.wikipedia.org/w/index.php?search=%s");
+
 define_webjump("bsc", "https://bugzilla.suse.com/show_bug.cgi?id=%s");
 define_webjump("bscn", "https://bugzilla.suse.com/buglist.cgi?cmdtype=runnamed&namedcmd=%s")
