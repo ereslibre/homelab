44421db0c986d4ae7a0906952074c07728e667a1
Rafael Fernández López <ereslibre@ereslibre.es>
2017-09-21 16:57:54 +0200

    Add some defaults

--- .conkerorrc/000-init.js
+++ .conkerorrc/000-init.js
@@ -2,6 +2,11 @@ url_completion_use_history = false;
 url_completion_use_bookmarks = true;
 can_kill_last_buffer = true;
 
+cwd = get_home_directory();
+cwd.append("Downloads");
+
+homepage = "about:blank"
+
 define_webjump("g", "http://www.google.com/search?q=%s");
 define_webjump("w", "https://en.wikipedia.org/w/index.php?search=%s");
 define_webjump("ws", "https://es.wikipedia.org/w/index.php?search=%s");
