1487b993c06a409c0ce0ead148836753683f1b6c
Rafael Fernández López <ereslibre@ereslibre.es>
2018-03-20 18:45:15 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,8 @@ set -x GOPATH $HOME/.go
 set -x CARGOPATH $HOME/.cargo
 
 set PATH /sbin /usr/sbin $HOME/.bin $GOPATH/bin $CARGOPATH/bin $PATH
+
+if type -q keychain; and not set -q WINDOW_MANAGER
+    keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
+    source $HOME/.keychain/(hostname)-fish
+end
