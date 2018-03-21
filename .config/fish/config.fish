551d88d86252c1b867c1dab5db12d853c5126687
Rafael Fernández López <ereslibre@ereslibre.es>
2018-03-21 09:10:19 +0100

    Add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,10 @@ set -x CARGOPATH $HOME/.cargo
 
 set PATH /sbin /usr/sbin $HOME/.bin $GOPATH/bin $CARGOPATH/bin $PATH
 
+alias iosc="osc -A https://api.suse.de"
+alias mutt="/usr/bin/mutt -F ~/.muttrc-gmail"
+alias smutt="/usr/bin/mutt -F ~/.muttrc-suse"
+
 if type -q keychain; and not set -q WINDOW_MANAGER
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
     source $HOME/.keychain/(hostname)-fish
