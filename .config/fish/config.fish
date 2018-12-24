71d904c936005aee1875302e1ff9762fff4f45ab
Rafael Fernández López <ereslibre@ereslibre.es>
2018-12-24 18:25:40 +0100

    Update settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,12 +7,11 @@ set -x EDITOR "emacs -nw -q"
 set -x GIT_EDITOR "emacs -nw -q"
 set -x GOPATH $HOME/projects/go
 set -x CARGOPATH $HOME/.cargo
+set -x SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F= '{print  $2}')
 
 set PATH /sbin /usr/sbin $HOME/.bin $GOPATH/bin $CARGOPATH/bin /usr/local/kubebuilder/bin $PATH
 
 alias iosc="osc -A https://api.suse.de"
-alias mutt="/usr/bin/mutt -F ~/.muttrc-gmail"
-alias smutt="/usr/bin/mutt -F ~/.muttrc-suse"
 
 if type -q keychain; and not set -q WINDOW_MANAGER
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
