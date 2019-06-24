738c61147647a75fea60d92d23b24556ae3d6f3a
Rafael Fernández López <ereslibre@ereslibre.es>
2019-06-24 20:25:22 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting
 
 cat ~/.todo
 
+set -x BEMENU_BACKEND wayland
 set -x TERMINAL tilix
 set -x EDITOR "emacsclient -t"
 set -x GIT_EDITOR "emacsclient -t"
@@ -9,9 +10,10 @@ set -x GOPATH $HOME/projects/go
 set -x CARGOPATH $HOME/.cargo
 set -x SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F= '{print  $2}')
 
-set PATH /sbin /usr/sbin $HOME/.bin $GOPATH/bin $CARGOPATH/bin /usr/local/kubebuilder/bin $PATH
+set PATH $GOPATH/bin /sbin /usr/sbin $HOME/.bin $CARGOPATH/bin $PATH
 
 alias emacs="emacsclient -t"
+alias outputs="swaymsg -t get_outputs"
 alias bs="docker run --rm -v ~/.config/osc/oscrc:/root/.config/osc/oscrc -v ~/projects/suse/ibs:/root/ibs -v ~/projects/suse/obs:/root/obs -it ereslibre/buildservice"
 
 if type -q keychain; and not set -q WINDOW_MANAGER
