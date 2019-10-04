f96f7610029e8fb76ba5b915b6e9bbb22a09fc96
Rafael Fernández López <ereslibre@ereslibre.es>
2019-10-04 14:36:26 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,11 +10,13 @@ set -x GOPATH $HOME/projects/go
 set -x CARGOPATH $HOME/.cargo
 set -x SSH_AUTH_SOCK (gnome-keyring-daemon --start | awk -F= '{print  $2}')
 
-set PATH $GOPATH/bin /sbin /usr/sbin $HOME/.bin $CARGOPATH/bin $PATH
+set PATH $GOPATH/bin /sbin /usr/sbin $HOME/.bin $CARGOPATH/bin /usr/bin/vendor_perl/ $HOME/.linkerd2/bin $PATH
 
+alias k="kubectl"
 alias emacs="emacsclient -t"
 alias outputs="swaymsg -t get_outputs"
 alias bs="docker run --rm -v ~/.config/osc/oscrc:/root/.config/osc/oscrc -v ~/projects/suse/ibs:/root/ibs -v ~/projects/suse/obs:/root/obs -it ereslibre/buildservice"
+alias tunnel-quassel="tunnel 4242 127.0.0.1 4242"
 
 if type -q keychain; and not set -q WINDOW_MANAGER
     keychain --nogui $HOME/.ssh/id_rsa ^/dev/null
