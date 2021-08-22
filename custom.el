4225a7c2af38c9f0d0897252536e015c86384631
Rafael Fernández López <ereslibre@ereslibre.es>
2021-08-22 20:42:56 +0200

    Update

---
+++ custom.el
@@ -0,0 +1,63 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face 'default)
+ '(custom-safe-themes
+   '("1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" default))
+ '(delete-selection-mode nil)
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   '(("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100)))
+ '(magit-diff-use-overlays nil)
+ '(org-agenda-files
+   '("~/org/inbox.org" "~/org/projects.org" "~/org/tickler.org" "~/org/reminders.org" "~/org/someday.org" "~/org/journal.org" "~/org/habits.org"))
+ '(package-selected-packages
+   '(ace-window carbon-now-sh use-package rego-mode bats-mode zig-mode monokai-theme doom-themes typescript-mode terraform-mode nix-mode adoc-mode helm-lsp protobuf-mode git-link yasnippet powerline helm-projectile helm helm-company projectile groovy-mode lsp-mode company yaml-mode yafolding vue-mode undo-tree rust-mode rainbow-delimiters neotree markdown-mode magit json-mode haskell-mode google-translate go-mode browse-kill-ring ack ripgrep))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#FD971F" :height 150 :family "Fira Code"))))
+ '(org-level-2 ((t (:inherit variable-pitch :foreground "#A6E22E" :height 150 :family "Fira Code"))))
+ '(org-level-3 ((t (:inherit variable-pitch :foreground "#66D9EF" :height 150 :family "Fira Code"))))
+ '(org-level-4 ((t (:inherit variable-pitch :foreground "#E6DB74" :height 150 :family "Fira Code"))))
+ '(org-level-5 ((t (:inherit variable-pitch :foreground "#A1EFE4" :height 150 :family "Fira Code"))))
+ '(org-level-6 ((t (:inherit variable-pitch :foreground "#A6E22E" :height 150 :family "Fira Code"))))
+ '(org-level-7 ((t (:inherit variable-pitch :foreground "#F92672" :height 150 :family "Fira Code"))))
+ '(org-level-8 ((t (:inherit variable-pitch :foreground "#66D9EF" :height 150 :family "Fira Code")))))
--- init.el
+++ init.el
@@ -20,8 +20,7 @@
 (with-eval-after-load 'doc-view
   (setq doc-view-resolution 300))
 
-(use-package magit
-  :defer 3)
+(use-package magit)
 
 (use-package yasnippet
   :config
@@ -33,7 +32,6 @@
   (setq git-link-use-commit t))
 
 (use-package company
-  :demand
   :config
   (add-hook 'after-init-hook 'global-company-mode))
 
@@ -67,23 +65,19 @@
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package helm
-  :demand
-  :bind (("C-x C-f" . helm-find-files)
+  :bind (("C-i" . helm-execute-persistent-action)
+         ("C-z" . helm-select-action)
+         ("C-x C-f" . helm-find-files)
          ("M-x" . helm-M-x)
          ([remap occur] . helm-occur)
          ([remap list-buffers] . helm-buffers-list)
          ([remap dabbrev-expand] . helm-dabbrev))
   :config
   (helm-mode 1)
-  (helm-autoresize-mode t)
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-  (define-key helm-map (kbd "C-z")  'helm-select-action))
+  (helm-autoresize-mode t))
 
 (use-package projectile
-  :demand
-  :bind-keymap ("C-c p" . projectile-command-map)
-  :after (helm)
+  :bind (("C-c p" . projectile-command-map))
   :config
   (projectile-mode +1)
   (setq projectile-completion-system 'helm)
