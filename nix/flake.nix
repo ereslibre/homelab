017dc1ef1a4fa02a24a54743c7ca8d09e858818d
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-02 16:44:53 +0100

    Update

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,9 +2,9 @@
   description = "A Home Manager flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
