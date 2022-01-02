7dd53b29253df6d15d5ece16bc7732e143d08e46
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-02 18:56:08 +0100

    Update

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -3,21 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: {
+  outputs = { self, nixpkgs, flake-utils, home-manager }: {
+    defaultApp.x86_64-darwin = flake-utils.lib.mkApp { drv = nixpkgs.legacyPackages.x86_64-darwin.home-manager; };
+
     homeConfigurations = {
-      "ereslibre@Rafaels-MacBook-Air.local" = inputs.home-manager.lib.homeManagerConfiguration {
+      "ereslibre@Rafaels-MacBook-Air" = home-manager.lib.homeManagerConfiguration {
         system = "x86_64-darwin";
         homeDirectory = "/Users/ereslibre";
         username = "ereslibre";
-        configuration = { config, pkgs, ... }: {
-          imports = [ ./common.nix ];
-        };
+        configuration.imports = [ ./home.nix ];
       };
     };
   };
---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,7 @@
+{
+  programs = {
+    emacs = {
+      enable = true;
+    };
+  };
+};
