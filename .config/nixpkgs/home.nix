70fb5a607591ffdfc00f2b66ffd197826d829550
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-03 01:11:07 +0100

    Update

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -7,12 +7,12 @@
       enable = true;
       shellAliases = {
         dir = "dir --color=auto";
-        emacs = "~/.nix-profile/bin/emacsclient --socket-name=main -t";
+        emacs = "${pkgs.emacs}/bin/emacsclient --socket-name=main -t";
         egrep = "egrep --color=auto";
         fgrep = "fgrep --color=auto";
-        gpg = "gpg --no-autostart";
+        gpg = "${pkgs.gnupg}/bin/gpg --no-autostart";
         grep = "grep --color=auto";
-        k = "kubectl";
+        k = "${pkgs.kubectl}/bin/kubectl";
         l = "ls --color=auto -CF";
         ll = "ls --color=auto -alF";
         la = "ls --color=auto -A";
