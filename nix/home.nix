d6391d45338145fdaf984e931f653011e628b065
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-03 21:45:53 +0100

    Update

--- nix/home.nix
+++ nix/home.nix
@@ -3,16 +3,42 @@
   home.packages = import ./packages.nix { inherit pkgs; };
 
   programs = {
+    direnv = {
+      enable = true;
+    };
+
     emacs = {
+      enable = true;
+    };
+
+    fzf = {
+      enable = true;
+    };
+
+    keychain = {
       enable = true;
     };
 
     zsh = {
       enable = true;
+      envExtra = ''
+        EDITOR="${pkgs.emacs}/bin/emacsclient --socket-name=main -t"
+        GOPATH=$(${pkgs.go}/bin/go env GOPATH)
+        GOROOT=$(${pkgs.go}/bin/go env GOROOT)
+        GO111MODULE=on
+        LANG='en_US.UTF-8'
+        PATH=''${HOME}/.bin:''${HOME}/.cargo/bin:''${HOME}/go/bin:/usr/local/bin:''${PATH}
+      '';
+      initExtra = ''
+        token() {
+          ${pkgs.yubikey-manager}/bin/ykman oath accounts code | grep -i "$1"
+        }
+      '';
       oh-my-zsh = {
         enable = true;
         theme = "ereslibre-af-magic";
         custom = "$HOME/dotfiles/.oh-my-zsh";
+        plugins = ["git"];
       };
       shellAliases = {
         dir = "dir --color=auto";
