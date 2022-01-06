8c0a609819f351f87b689c4f9f69e03bf8f41e10
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-06 21:26:07 +0100

    Update

--- home.nix
+++ home.nix
@@ -6,6 +6,28 @@
         executable = true;
       };
       # Make it conditional on Mac only
+      "${config.home.homeDirectory}/Library/LaunchAgents/es.ereslibre.emacs.plist".text =
+        ''
+          <?xml version="1.0" encoding="UTF-8"?>
+          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+          <plist version="1.0">
+          <dict>
+            <key>Label</key>
+            <string>es.ereslibre.es.emacs</string>
+            <key>ProgramArguments</key>
+            <array>
+              <string>${pkgs.emacs}/bin/emacs</string>
+              <string>--fg-daemon</string>
+            </array>
+            <key>RunAtLoad</key>
+            <true/>
+            <key>KeepAlive</key>
+            <true/>
+            <key>LSUIElement</key>
+            <true/>
+          </dict>
+          </plist>
+                '';
       ".bin/rosetta" = {
         source = ./assets/mac/rosetta;
         executable = true;
