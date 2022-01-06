ee074b6ac3589cdda0b23fa5fb5c2f5dc84f9e29
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-06 21:41:39 +0100

    Update

--- home.nix
+++ home.nix
@@ -1,4 +1,35 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  macFiles = if pkgs.stdenv.isDarwin then {
+    "${config.home.homeDirectory}/Library/LaunchAgents/es.ereslibre.emacs.plist".text =
+      ''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>es.ereslibre.es.emacs</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>${pkgs.emacs}/bin/emacs</string>
+            <string>--fg-daemon</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>LSUIElement</key>
+          <true/>
+        </dict>
+        </plist>
+              '';
+    ".bin/rosetta" = {
+      source = ./assets/mac/rosetta;
+      executable = true;
+    };
+  } else
+    { };
+in {
   home = {
     file = {
       ".bin/emacsclient" = {
@@ -13,35 +44,7 @@
       ".gitconfig.suse".source = ./assets/git/config-suse;
       ".ssh/config".source = ./assets/ssh/config;
       ".tmux.conf".source = ./assets/tmux/config;
-    } // (if pkgs.stdenv.isDarwin then {
-      "${config.home.homeDirectory}/Library/LaunchAgents/es.ereslibre.emacs.plist".text =
-        ''
-          <?xml version="1.0" encoding="UTF-8"?>
-          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-          <plist version="1.0">
-          <dict>
-            <key>Label</key>
-            <string>es.ereslibre.es.emacs</string>
-            <key>ProgramArguments</key>
-            <array>
-              <string>${pkgs.emacs}/bin/emacs</string>
-              <string>--fg-daemon</string>
-            </array>
-            <key>RunAtLoad</key>
-            <true/>
-            <key>KeepAlive</key>
-            <true/>
-            <key>LSUIElement</key>
-            <true/>
-          </dict>
-          </plist>
-                '';
-      ".bin/rosetta" = {
-        source = ./assets/mac/rosetta;
-        executable = true;
-      };
-    } else
-      { });
+    } // macFiles;
 
     packages = import ./packages.nix { inherit pkgs; };
   };
