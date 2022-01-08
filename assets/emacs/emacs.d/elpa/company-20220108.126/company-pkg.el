d1a7a3613709f33d7afed7ba37c9d67a5ca95a93
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-08 10:07:43 +0100

    Update

---
+++ assets/emacs/emacs.d/elpa/company-20220108.126/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20220108.126" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "8e4716172a2ba7fdd3f1d37096de88142ebbcc8d" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
