42b43d61e4017d72f479b0aec96e7f2143f99c07
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-08 12:59:28 +0100

    Update

--- assets/emacs/emacs.d/init.el
+++ assets/emacs/emacs.d/init.el
@@ -46,7 +46,7 @@
   (setq gc-cons-threshold 100000000)
   (setq read-process-output-max (* 1024 1024))
   (setq lsp-auto-configure t)
-  (setq lsp-restart 'auto-restart)
+  (setq lsp-restart 'interactive)
   (add-to-list 'lsp-language-id-configuration '(rego-mode . "rego"))
   (add-to-list 'lsp-language-id-configuration '(emacs-lisp-mode . "el"))
   (add-hook 'prog-mode-hook 'lsp))
