bd79212b41d215a97a5083b3e992cb8d5d148425
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-10 22:37:36 +0200

    Update

---
+++ dotfiles.nix
@@ -0,0 +1,17 @@
+{ config, pkgs }:
+let macFiles = import ./mac.nix { inherit config pkgs; };
+in {
+  ".bin/emacsclient" = {
+    source = ./assets/emacs/emacsclient;
+    executable = true;
+  };
+  ".emacs.d" = {
+    source = ./assets/emacs/emacs.d;
+    recursive = true;
+  };
+  ".gitconfig".source = ./assets/git/config;
+  ".gitconfig.suse".source = ./assets/git/config-suse;
+  ".gitignore".source = ./assets/git/gitignore;
+  ".ssh/config".source = ./assets/ssh/config;
+  ".tmux.conf".source = ./assets/tmux/config;
+} // macFiles
--- home.nix
+++ home.nix
@@ -1,52 +1,6 @@
-{ config, pkgs, ... }:
-let
-  macFiles = if pkgs.stdenv.isDarwin then {
-    "${config.home.homeDirectory}/Library/LaunchAgents/es.ereslibre.emacs.plist".text =
-      ''
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>es.ereslibre.es.emacs</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>${pkgs.emacs}/bin/emacs</string>
-            <string>--fg-daemon</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>LSUIElement</key>
-          <true/>
-        </dict>
-        </plist>
-              '';
-    ".bin/rosetta" = {
-      source = ./assets/mac/rosetta;
-      executable = true;
-    };
-  } else
-    { };
-in {
+{ config, pkgs, ... }: {
   home = {
-    file = {
-      ".bin/emacsclient" = {
-        source = ./assets/emacs/emacsclient;
-        executable = true;
-      };
-      ".emacs.d" = {
-        source = ./assets/emacs/emacs.d;
-        recursive = true;
-      };
-      ".gitconfig".source = ./assets/git/config;
-      ".gitconfig.suse".source = ./assets/git/config-suse;
-      ".gitignore".source = ./assets/git/gitignore;
-      ".ssh/config".source = ./assets/ssh/config;
-      ".tmux.conf".source = ./assets/tmux/config;
-    } // macFiles;
-
+    file = import ./dotfiles.nix { inherit config pkgs; };
     packages = import ./packages.nix { inherit pkgs; };
   };
 
