7df90d8e5c21a6e442cdce85fc7516eb15145cf4
Rafael Fernández López <ereslibre@ereslibre.es>
2022-01-19 14:56:03 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -28,27 +28,10 @@
               keys = [ ];
               inheritType = "any";
             };
-            # Bash is required because is the hardcoded `runtimeShell`
-            # used by the emacs service user unit:
-            # https://github.com/nix-community/home-manager/blob/c751aeb19e84a0a777f36fd5ea73482a066bb406/modules/services/emacs.nix#L116.
-            #
-            # This is so the emacs service is started in a login shell
-            # that sources the PATH and the nix profile.
-            #
-            # It would be nice to generalize that and remove the hardcoded need for bash, coming from nixpkgs -- all-packages.nix --:
-            #
-            # ```nix
-            # runtimeShell = "${runtimeShellPackage}${runtimeShellPackage.shellPath}";
-            # runtimeShellPackage = bash;
-            # ```
             bash = {
-              enable = true;
               profileExtra = ''
                 if [ -e ''${HOME}/.nix-profile/etc/profile.d/nix.sh ]; then . ''${HOME}/.nix-profile/etc/profile.d/nix.sh; fi
                 if [ -e ''${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then . ''${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh; fi
-              '';
-              initExtra = ''
-                exec ''${HOME}/.nix-profile/bin/zsh
               '';
             };
             zsh = {
