48b10a9934a5c0ba9079bc48d9b199035207ba38
Rafael Fernández López <ereslibre@ereslibre.es>
2022-07-03 12:25:16 +0300

    Add update-emacs-d script

--- Makefile
+++ Makefile
@@ -14,3 +14,6 @@ fmt:
 
 lint:
 	nix develop --command nix-linter -r
+
+update-emacs-d:
+	scripts/update-emacs-d.sh
---
+++ scripts/update-emacs-d.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -x
+
+emacsclient -e "(kill-emacs)"
+rm -rf assets/emacs/emacs.d/elpa
+mkdir -p assets/emacs/emacs.d/elpa
+make
+~/.nix-profile/bin/emacs --batch --debug-init --load ~/.emacs.d/early-init.el --load ~/.emacs.d/init.el -f package-refresh-contents
+~/.nix-profile/bin/emacs --batch --debug-init --load ~/.emacs.d/early-init.el --load ~/.emacs.d/init.el -f kill-emacs
+cp -r ~/.emacs.d/elpa assets/emacs/emacs.d
+rm -rf ~/.emacs.d/elpa
+git add assets/emacs
+git commit -m 'Update emacs packages'
+make
