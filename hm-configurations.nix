69fc47ff23cd03d447a48cb30d7ffe07a0f1e8e6
Rafael Fernández López <ereslibre@ereslibre.es>
2022-07-18 09:55:56 +0200

    Update

--- hm-configurations.nix
+++ hm-configurations.nix
@@ -29,59 +29,6 @@ let
       username = "ereslibre";
       configuration = macbookSharedConfiguration homeDirectory;
     });
-  desktopConfiguration = home-manager.lib.homeManagerConfiguration {
-    system = "x86_64-linux";
-    homeDirectory = "/home/ereslibre";
-    username = "ereslibre";
-    configuration = {
-      imports = [ ./home.nix ];
-      # Enabling linger makes the systemd user services start
-      # automatically. In this machine, I want to trigger the
-      # `gpg-forward-agent-path` service file automatically as
-      # systemd starts, so the socket dir is always created and I
-      # can forward the GPG agent through SSH directly without
-      # having a first failed connection due to a missing
-      # `/run/user/<id>/gnupg`.
-      home.activation.linger =
-        home-manager.lib.hm.dag.entryBefore [ "reloadSystemd" ] ''
-          loginctl enable-linger $USER
-        '';
-      # This service creates the GPG socket dir (`/run/user/<id>/gnupg`) automatically.
-      systemd.user.services = {
-        "gpg-forward-agent-path" = {
-          Unit.Description = "Create GnuPG socket directory";
-          Service = {
-            ExecStart =
-              "${nixpkgs.legacyPackages.x86_64-linux.gnupg}/bin/gpgconf --create-socketdir";
-            ExecStop = "";
-          };
-          Install.WantedBy = [ "default.target" ];
-        };
-      };
-      programs = nixpkgs.lib.recursiveUpdate (commonConfiguration {
-        emacsClient =
-          "${nixpkgs.legacyPackages.x86_64-linux.emacs}/bin/emacsclient -s $XDG_RUNTIME_DIR/emacs/server -t";
-      }) {
-        keychain = {
-          keys = [ ];
-          inheritType = "any";
-        };
-        zsh = {
-          shellAliases = {
-            gpg =
-              "${nixpkgs.legacyPackages.x86_64-linux.gnupg}/bin/gpg --no-autostart";
-          };
-        };
-      };
-      services.emacs = {
-        enable = true;
-        socketActivation.enable = true;
-      };
-    };
-  };
 in {
   "ereslibre@Rafaels-Air" = macbookConfiguration;
-  "ereslibre@MacBook-Air" = macbookConfiguration;
-  "ereslibre@Rafaels-MacBook-Air" = macbookConfiguration;
-  "ereslibre@desktop" = desktopConfiguration;
 }
