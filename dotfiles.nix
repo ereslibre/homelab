1e2a70410dc8b4999a5201414c1d1dcbd804f8c3
Rafael Fernández López <ereslibre@ereslibre.es>
2022-07-18 09:52:20 +0200

    Update

--- dotfiles.nix
+++ dotfiles.nix
@@ -6,7 +6,6 @@ in {
     recursive = true;
   };
   ".gitconfig".source = ./assets/git/config;
-  ".gitconfig.suse".source = ./assets/git/config-suse;
   ".gitignore".source = ./assets/git/gitignore;
   ".ssh/config".source = ./assets/ssh/config;
   ".tmux.conf".source = ./assets/tmux/config;
