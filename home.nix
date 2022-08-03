7c6fbc2323f839cd811b68c0ab0491082a85ed07
Rafael Fernández López <ereslibre@ereslibre.es>
2022-08-03 17:30:39 +0200

    Update

--- home.nix
+++ home.nix
@@ -2,6 +2,16 @@
   home = {
     file = import ./dotfiles.nix { inherit pkgs; };
     packages = import ./packages.nix { inherit pkgs; };
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
   };
 
   programs = {
