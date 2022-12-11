ab9845ab5f828f74238edf280761aa9396d0547d
Rafael Fernández López <ereslibre@ereslibre.es>
2022-12-11 23:48:52 +0100

    Update

--- hm-configurations.nix
+++ hm-configurations.nix
@@ -72,7 +72,7 @@ let
         # `/run/user/<id>/gnupg`.
         home.activation.linger =
           home-manager.lib.hm.dag.entryBefore [ "reloadSystemd" ] ''
-            sudo loginctl enable-linger $USER
+            loginctl enable-linger $USER
           '';
 
         # This service creates the GPG socket dir (`/run/user/<id>/gnupg`) automatically.
