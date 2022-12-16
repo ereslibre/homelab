7d5afd356378b719741844f7c42c69bf4db29d1c
Rafael Fernández López <ereslibre@ereslibre.es>
2022-12-16 21:13:37 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -4,21 +4,14 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    # nixpkgs revision known to work -- https://channels.nix.gsc.io
-    nixpkgs-rpi.url =
-      "github:nixos/nixpkgs/72f492e275fc29d44b3a4daf952fbeffc4aed5b8";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager-rpi = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs-rpi";
-    };
   };
 
   outputs =
-    { flake-utils, home-manager, home-manager-rpi, nixpkgs, nixpkgs-rpi, ... }:
+    { flake-utils, home-manager, nixpkgs, ... }:
     flake-utils.lib.eachSystem
     (flake-utils.lib.defaultSystems ++ [ "aarch64-darwin" ]) (system:
       let pkgs = nixpkgs.legacyPackages.${system};
@@ -27,14 +20,10 @@
           buildInputs = with pkgs; [ cachix nix-linter nixfmt ];
         };
       }) // {
-        # Re-export home-manager, home-manager-rpi, nixpkgs and nixpkgs-rpi as a usable output
-        inherit home-manager home-manager-rpi nixpkgs nixpkgs-rpi;
+        # Re-export home-manager and nixpkgs as a usable output
+        inherit home-manager nixpkgs;
         # Export home-manager configurations
         homeConfigurations =
           import ./hm-configurations.nix { inherit home-manager nixpkgs; };
-        homeConfigurationsRpi = import ./hm-configurations.nix {
-          home-manager = home-manager-rpi;
-          nixpkgs = nixpkgs-rpi;
-        };
       };
 }
