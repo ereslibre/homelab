9d2c5c4f5cbbe33465db40e271f2ede2704f50c7
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-12 23:00:59 +0100

    Update

--- packages.nix
+++ packages.nix
@@ -1,49 +1,43 @@
 { devenv, pkgs }:
-[ devenv.packages.${pkgs.stdenv.system}.devenv ] ++ (with pkgs;
-  [
-    awscli
-    bat
-    binutils
-    cacert
-    clang-tools
-    coreutils
-    curl
-    diffutils
-    direnv
-    dive
-    file
-    fluxcd
-    fzf
-    gh
-    git
-    gnumake
-    gnupg
-    gnupg-pkcs11-scd
-    go
-    gopls
-    gotools
-    kubernetes-helm
-    htop
-    jq
-    just
-    keychain
-    kubectl
-    kubeseal
-    mtr
-    otpauth
-    reg
-    ripgrep
-    rlwrap
-    rnix-lsp
-    rust-analyzer
-    rustup
-    terraform
-    tree
-    velero
-    wget
-    xxd
-    yubikey-manager
-    zstd
-  ] ++ (with pkgs; lib.optionals stdenv.isLinux [ kube3d valgrind ])
-  ++ (with pkgs;
-    lib.optionals (stdenv.system != "aarch64-darwin") [ yq-go zbar ]))
+[ devenv.packages.${pkgs.stdenv.system}.devenv ] ++ (with pkgs; [
+  bat
+  binutils
+  cacert
+  coreutils
+  curl
+  diffutils
+  direnv
+  dive
+  file
+  fluxcd
+  fzf
+  gh
+  git
+  gnumake
+  gnupg
+  gnupg-pkcs11-scd
+  gopls
+  gotools
+  kubernetes-helm
+  htop
+  jq
+  just
+  keychain
+  kubectl
+  kubeseal
+  mtr
+  # otpauth
+  reg
+  ripgrep
+  rlwrap
+  rnix-lsp
+  rust-analyzer
+  rustup
+  terraform
+  tree
+  velero
+  wget
+  xxd
+  yubikey-manager
+  zstd
+]) ++ (with pkgs; lib.optionals stdenv.isLinux [ kube3d ])
