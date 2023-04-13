28f0d37c888ae06346394df8912b0d75ffe6b4a9
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-13 23:52:11 +0200

    Add note

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
     home-manager = {
+      # Use master until 23.05 is released due to current
+      # incompatibilities with nixpkgs/release-22.11 and
+      # home-manager/release-22.11. When 23.05 is released, change
+      # both
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
