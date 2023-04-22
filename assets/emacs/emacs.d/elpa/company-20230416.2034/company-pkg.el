a47580a32f7f28b5d8f1f106d9591bdf794fe37e
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-22 12:00:33 +0200

    Update

---
+++ assets/emacs/emacs.d/elpa/company-20230416.2034/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20230416.2034" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "8a78f320019574bc35b5727f95b052b27918da20" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ assets/emacs/emacs.d/elpa/helm-20230419.650/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20230419.650" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.0")
+    (popup "0.5.3"))
+  :commit "b2d0738bab027748e97a54a8cb465ed73f180631" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
