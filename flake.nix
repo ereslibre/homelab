6adfbca7e50731557e040e00d37d91906c316398
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-28 09:18:25 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -5,14 +5,7 @@
     devenv.url = "github:cachix/devenv/v0.6.2";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    home-manager = {
-      # Use master until 23.05 is released due to current
-      # incompatibilities with nixpkgs/release-22.11 and
-      # home-manager/release-22.11. When 23.05 is released, change
-      # both
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
   };
 
   outputs = { devenv, flake-utils, home-manager, nixpkgs, ... }:
