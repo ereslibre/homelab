df74c860bf3583163c5e53bedc102e1284350baf
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-28 09:48:11 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -5,10 +5,9 @@
     devenv.url = "github:cachix/devenv/v0.6.2";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-    nixpkgs-nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs-nixos-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
