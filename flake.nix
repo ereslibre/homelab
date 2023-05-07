9f898427346a9a3a1214e83b1ad7950d41eb4b31
Rafael Fernández López <ereslibre@ereslibre.es>
2023-05-07 09:55:32 +0200

    Refactor configuration a little bit

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     devenv.url = "github:cachix/devenv/v0.6.2";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- hm-configurations.nix
+++ hm-configurations.nix
@@ -16,104 +16,76 @@ let
       zsh = shellExtras;
     };
 
-  sharedConfiguration = { system, homeDirectory }: {
-    programs = programsConfiguration {
-      emacsClient = if nixpkgs.legacyPackages.${system}.stdenv.isDarwin then
-        "${
-          nixpkgs.legacyPackages.${system}.emacs-nox
-        }/bin/emacsclient -s ${homeDirectory}/.emacs.d/emacs.sock -t"
-      else
-        "${nixpkgs.legacyPackages.${system}.emacs-nox}/bin/emacsclient -t";
-    };
-    home.stateVersion = stateVersion;
-  };
-
-  macbookRawConfiguration = { system, username, homeDirectory, profile }: {
-    configuration = (nixpkgs.lib.mkMerge [
-      (import ./home.nix {
-        inherit devenv username profile;
-        config.home.homeDirectory = homeDirectory;
-        pkgs = nixpkgs.legacyPackages.${system};
-      })
-      (sharedConfiguration { inherit system homeDirectory; })
-    ]);
-  };
+  sharedConfiguration = { system, homeDirectory, ... }:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in [
+      {
+        programs = programsConfiguration {
+          emacsClient = if pkgs.stdenv.isDarwin then
+            "${pkgs.emacs-nox}/bin/emacsclient -s ${homeDirectory}/.emacs.d/emacs.sock -t"
+          else
+            "${pkgs.emacs-nox}/bin/emacsclient -t";
+        };
+      }
+      ./home.nix
+      { home.stateVersion = stateVersion; }
+    ];
 
   macbookConfiguration = { system, username, profile }:
-    let homeDirectory = "/Users/${username}";
-    in rec {
-      inherit (macbookRawConfiguration {
-        inherit system username homeDirectory profile;
-      })
-        configuration;
-      hm-config = home-manager.lib.homeManagerConfiguration rec {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          configuration
-          { home = { inherit username homeDirectory stateVersion; }; }
-        ];
-      };
-    };
-
-  workstationRawConfiguration =
-    { system, username, homeDirectory, profile }: rec {
-      configuration = let pkgs = nixpkgs.legacyPackages.${system};
-      in (nixpkgs.lib.mkMerge [
-        (import ./home.nix {
-          inherit devenv username profile pkgs;
-          config.home.homeDirectory = homeDirectory;
-        })
-        {
-          # Enabling linger makes the systemd user services start
-          # automatically. In this machine, I want to trigger the
-          # `gpg-forward-agent-path` service file automatically as
-          # systemd starts, so the socket dir is always created and I
-          # can forward the GPG agent through SSH directly without
-          # having a first failed connection due to a missing
-          # `/run/user/<id>/gnupg`.
-          home.activation.linger =
-            home-manager.lib.hm.dag.entryBefore [ "reloadSystemd" ] ''
-              ${pkgs.systemd}/bin/loginctl enable-linger $USER
-            '';
-
-          # This service creates the GPG socket dir (`/run/user/<id>/gnupg`) automatically.
-          systemd.user.services = {
-            "gpg-forward-agent-path" = {
-              Unit.Description = "Create GnuPG socket directory";
-              Service = {
-                ExecStart = "${pkgs.gnupg}/bin/gpgconf --create-socketdir";
-                ExecStop = "";
-              };
-              Install.WantedBy = [ "default.target" ];
-            };
-          };
-
-          programs.keychain = {
-            keys = [ ];
-            inheritType = "any";
-          };
-
-          programs.zsh.shellAliases = {
-            gpg = "${pkgs.gnupg}/bin/gpg --no-autostart";
-          };
-        }
-        (sharedConfiguration { inherit system homeDirectory; })
-      ]);
+    machineConfiguration {
+      homeDirectory = "/Users/${username}";
+      inherit system username profile;
     };
 
   workstationConfiguration = { system, username, profile }:
-    let homeDirectory = "/home/${username}";
-    in rec {
-      inherit (workstationRawConfiguration {
-        inherit system username homeDirectory profile;
-      })
-        configuration;
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in machineConfiguration {
+      homeDirectory = "/home/${username}";
+      hmModules = [{
+        # Enabling linger makes the systemd user services start
+        # automatically. In this machine, I want to trigger the
+        # `gpg-forward-agent-path` service file automatically as
+        # systemd starts, so the socket dir is always created and I
+        # can forward the GPG agent through SSH directly without
+        # having a first failed connection due to a missing
+        # `/run/user/<id>/gnupg`.
+        home.activation.linger =
+          home-manager.lib.hm.dag.entryBefore [ "reloadSystemd" ] ''
+            ${pkgs.systemd}/bin/loginctl enable-linger $USER
+          '';
+
+        # This service creates the GPG socket dir (`/run/user/<id>/gnupg`) automatically.
+        systemd.user.services = {
+          "gpg-forward-agent-path" = {
+            Unit.Description = "Create GnuPG socket directory";
+            Service = {
+              ExecStart = "${pkgs.gnupg}/bin/gpgconf --create-socketdir";
+              ExecStop = "";
+            };
+            Install.WantedBy = [ "default.target" ];
+          };
+        };
+
+        programs.keychain = {
+          keys = [ ];
+          inheritType = "any";
+        };
+
+        programs.zsh.shellAliases = {
+          gpg = "${pkgs.gnupg}/bin/gpg --no-autostart";
+        };
+      }];
+      inherit system username profile;
+    };
+
+  machineConfiguration =
+    { system, username, homeDirectory, profile, hmModules ? [ ] }: {
       hm-config = home-manager.lib.homeManagerConfiguration rec {
         pkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          configuration
-          { home = { inherit username homeDirectory stateVersion; }; }
-        ];
+        extraSpecialArgs = { inherit devenv username profile pkgs; };
+        modules = hmModules
+          ++ (sharedConfiguration { inherit system homeDirectory; })
+          ++ [{ home = { inherit username homeDirectory stateVersion; }; }];
       };
     };
 in {
