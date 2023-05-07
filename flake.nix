307230da7fe60f44ea4900c79107e48ad39ec4dc
Rafael Fernández López <ereslibre@ereslibre.es>
2023-05-07 08:48:05 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -4,25 +4,19 @@
   inputs = {
     devenv.url = "github:cachix/devenv/v0.6.2";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/master";
-    nixpkgs-release-22-11.url = "github:nixos/nixpkgs/release-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager = {
-      # Use master, given it is the only branch that contains
-      # f69816489d5bcd1329c50fb4a7035a9a9dc19a3b. This commit is
-      # necessary for dotfiles pass CI, because it creates missing
-      # directories that are not longer created by Nix 2.14.
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    { devenv, flake-utils, home-manager, nixpkgs, nixpkgs-release-22-11, ... }:
+  outputs = { devenv, flake-utils, home-manager, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with nixpkgs-release-22-11.legacyPackages.${system}; [
+          buildInputs = with nixpkgs.legacyPackages.${system}; [
             cachix
             nix-linter
             nixfmt
@@ -30,7 +24,7 @@
         };
       }) // {
         # Re-export devenv, home-manager and nixpkgs as usable outputs
-        inherit devenv home-manager nixpkgs nixpkgs-release-22-11;
+        inherit devenv home-manager nixpkgs;
         # Export home-manager configurations
         homeConfigurations = import ./hm-configurations.nix {
           inherit devenv home-manager nixpkgs;
