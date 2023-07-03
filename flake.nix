a539ee4d5139bd77e86c895879599c2c09a490e3
Rafael Fernández López <rfernandezl@vmware.com>
2023-07-03 18:26:29 +0200

    devenv: 0.6.2 -> 0.6.3

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Sweet Home";
 
   inputs = {
-    devenv.url = "github:cachix/devenv/v0.6.2";
+    devenv.url = "github:cachix/devenv/v0.6.3";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     home-manager = {
