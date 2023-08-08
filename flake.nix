5e161bb9edf0a97759116c745639c069c085f7d4
Rafael Fernández López <ereslibre@ereslibre.es>
2023-08-09 00:07:47 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     devenv.url = "github:cachix/devenv/v0.6.3";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
   };
 
   outputs = {
@@ -30,8 +30,8 @@
       };
     })
     // {
-      # Re-export devenv, home-manager and nixpkgs as usable outputs
-      inherit devenv home-manager nixpkgs;
+      # Re-export devenv, flake-utils, home-manager and nixpkgs as usable outputs
+      inherit devenv flake-utils home-manager nixpkgs;
       # Export home-manager configurations
       homeConfigurations = import ./hm-configurations.nix {
         inherit devenv home-manager nixpkgs;
