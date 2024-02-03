24f1b7e2f047b7825266e75317b33014f7c43706
Rafael Fernández López <ereslibre@ereslibre.es>
2024-02-03 19:06:06 +0100

    Update

---
+++ assets/emacs/emacs.d/elpa/adoc-mode-20240201.2055/adoc-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "adoc-mode" "20240201.2055" "a major-mode for editing AsciiDoc files"
+  '((emacs "26"))
+  :commit "d4df8e1de68699fa9bc45bd4486b49f8b53985f6" :authors
+  '(("Florian Kaufmann" . "sensorflo@gmail.com"))
+  :maintainers
+  '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("docs" "wp")
+  :url "https://github.com/bbatsov/adoc-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ assets/emacs/emacs.d/elpa/json-mode-20240203.751/json-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "json-mode" "20240203.751" "Major mode for editing JSON files"
+  '((json-snatcher "1.0.0")
+    (emacs "24.4"))
+  :commit "bfd1557aaa20b7518b808fdc869f094b52205234" :authors
+  '(("Josh Johnston")
+    ("taku0"))
+  :maintainers
+  '(("Josh Johnston"))
+  :maintainer
+  '("Josh Johnston")
+  :url "https://github.com/joshwnj/json-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
