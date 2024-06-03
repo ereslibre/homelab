818851c5a80a18967f260a8b6e476e7294200017
Rafael Fernández López <ereslibre@ereslibre.es>
2024-06-03 16:15:37 +0200

    Update

--- packages.nix
+++ packages.nix
@@ -26,7 +26,7 @@
   ];
   global-language-tools = with pkgs; [gopls gotools rustup];
   kubernetes-tools = with pkgs; ([fluxcd kubectl kubernetes-helm kubeseal velero] ++ (lib.optionals pkgs.stdenv.isLinux [kind kube3d]));
-  platform-tools = with pkgs; [gh opentofu];
+  platform-tools = with pkgs; [gh];
 in {
   home.packages =
     container-tools
