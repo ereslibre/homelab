cfdebb6e583eec6e20ee9d7417db4e1813b4c45c
Rafael Fernández López <ereslibre@ereslibre.es>
2024-06-10 23:22:06 +0200

    Update

--- programs.nix
+++ programs.nix
@@ -17,7 +17,13 @@
   emacsBinary = {nox}:
     if mainlyRemote || nox
     then "${maybeWrappedEmacsClient pkgs.emacs-nox}/bin/emacsclient --tty"
-    else "${maybeWrappedEmacsClient pkgs.emacs}/bin/emacsclient --create-frame --no-wait";
+    else
+      (let
+        script = pkgs.writeShellScriptBin "emacsclient" ''
+          #! ${pkgs.runtimeShell} -e
+          exec ${maybeWrappedEmacsClient pkgs.emacs}/bin/emacsclient --create-frame --no-wait -e "(progn (select-frame-set-input-focus (selected-frame)) (toggle-frame-maximized) (find-file (expand-file-name \"$1\")))"
+        '';
+      in "${script}/bin/emacsclient");
   emacs = {nox}: emacsBinary {inherit nox;};
   k = "${pkgs.kubectl}/bin/kubectl";
   shellExtras = {
