a7392cca5a0e29a22a5b1adb3f736075e1ccd663
Rafael Fernández López <ereslibre@ereslibre.es>
2024-06-14 15:11:48 +0200

    Update

--- programs.nix
+++ programs.nix
@@ -44,7 +44,6 @@ in {
     ];
     sessionVariables = {
       EDITOR = emacs {nox = true;};
-      TERM = "xterm-direct";
     };
   };
   programs = {
@@ -167,11 +166,7 @@ in {
       enable = true;
       enableCompletion = false;
       envExtra = ''
-        export TERM=${
-          if pkgs.stdenv.isLinux
-          then "xterm-direct"
-          else "xterm-256color"
-        }
+        export TERM=xterm-direct
         export GIT_EDITOR="${emacs {nox = true;}}"
         export GOPATH="${config.home.homeDirectory}/.go"
         export PATH="${config.home.homeDirectory}/.bin:${config.home.homeDirectory}/.go/bin:${config.home.homeDirectory}/.cargo/bin:''${PATH}"
