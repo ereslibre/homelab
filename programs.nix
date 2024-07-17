69935ca67ad5c05e4d08f2135092065ed01c89c1
Rafael Fernández López <ereslibre@ereslibre.es>
2024-07-17 11:27:03 +0200

    Update

--- programs.nix
+++ programs.nix
@@ -149,29 +149,23 @@ in {
     ripgrep.enable = true;
     ssh = {
       enable = true;
-      extraConfig = ''
-        Host ereslibre-1.oracle.cloud ereslibre-2.oracle.cloud
-            User                  ubuntu
-
-        Host hulk hulk.ereslibre.net nuc-1 nuc-1.ereslibre.net nuc-2 nuc-2.ereslibre.net nuc-3 nuc-3.ereslibre.net
-            ForwardAgent          yes
-            RemoteForward         /run/user/1000/gnupg/S.gpg-agent /Users/${username}/.gnupg/S.gpg-agent.extra
-
-        Host *.ereslibre.local *.ereslibre.net
-            ForwardAgent          yes
-
-        Host 10.0.*
-            ForwardAgent          yes
-
-        Host *
-            User                  ereslibre
-            Compression           yes
-            ExitOnForwardFailure  yes
-            ForwardX11            no
-            ServerAliveCountMax   10
-            ServerAliveInterval   20
-            TCPKeepAlive          no
-      '';
+      forwardAgent = true;
+      matchBlocks = {
+        "ereslibre-1.oracle.cloud ereslibre-2.oracle.cloud" = {
+          user = "ubuntu";
+        };
+        "hulk hulk.ereslibre.net nuc-1 nuc-1.ereslibre.net nuc-2 nuc-2.ereslibre.net nuc-3 nuc-3.ereslibre.net" = {
+          remoteForward = "/run/user/1000/gnupg/S.gpg-agent /Users/${username}/.gnupg/S.gpg-agent.extra";
+        };
+        "*" = {
+          compression = "yes";
+          exitOnForwardFailure = "yes";
+          forwardX11 = "no";
+          serverAliveCountMax = "10";
+          serverAliveInterval = "20";
+          tcpKeepAlive = "no";
+        };
+      };
     };
     starship = {
       enable = true;
