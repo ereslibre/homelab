669cc2a3250066f4d52ff336239dcfea7a1b63ae
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-09 14:06:15 +0100

    Update

--- dotfiles/packages.nix
+++ dotfiles/packages.nix
@@ -1,32 +1,28 @@
 {pkgs, ...}: let
   container-tools = with pkgs; ([dive reg regctl] ++ lib.optionals pkgs.stdenv.isLinux [distrobox]);
-  core-tools = with pkgs;
-    [
-      binutils
-      coreutils
-      curl
-      devenv
-      diffutils
-      dig
-      file
-      gnumake
-      gnupg
-      just
-      mtr
-      gnupg-pkcs11-scd
-      otpauth
-      ripgrep
-      rlwrap
-      tree
-      watch
-      wget
-      xxd
-      zstd
-    ]
-    ++ (
-      # Due to https://github.com/NixOS/nixpkgs/issues/349449
-      lib.optionals pkgs.stdenv.isLinux (with pkgs; [mosh])
-    );
+  core-tools = with pkgs; [
+    binutils
+    coreutils
+    curl
+    devenv
+    diffutils
+    dig
+    file
+    gnumake
+    gnupg
+    just
+    mosh
+    mtr
+    gnupg-pkcs11-scd
+    otpauth
+    ripgrep
+    rlwrap
+    tree
+    watch
+    wget
+    xxd
+    zstd
+  ];
   global-language-tools = with pkgs; [gopls gotools rustup];
   kubernetes-tools = with pkgs; ([fluxcd kubectl kubernetes-helm kubeseal velero] ++ (lib.optionals pkgs.stdenv.isLinux [kind kube3d]));
   platform-tools = with pkgs; [gh];
