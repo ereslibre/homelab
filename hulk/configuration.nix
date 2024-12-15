63ca9805d3260fe73f7a0a7013620a7a40dc75ee
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-15 12:53:21 +0100

    Update

--- hulk/configuration.nix
+++ hulk/configuration.nix
@@ -30,23 +30,10 @@
     !include ${config.sops.secrets.nix-access-tokens.path}
   '';
 
-  services = {
-    ollama = {
-      enable = true;
-      host = "0.0.0.0";
-      loadModels = ["qwen2.5-coder:32b"];
-    };
-    open-webui = {
-      enable = true;
-      host = "0.0.0.0";
-      environment = {
-        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-        WEBUI_AUTH = "False";
-        ANONYMIZED_TELEMETRY = "False";
-        DO_NOT_TRACK = "True";
-        SCARF_NO_ANALYTICS = "True";
-      };
-    };
+  services.ollama = {
+    enable = true;
+    host = "0.0.0.0";
+    loadModels = ["qwen2.5-coder:32b"];
   };
 
   # This value determines the NixOS release from which the default
--- nuc-3/configuration.nix
+++ nuc-3/configuration.nix
@@ -32,6 +32,18 @@
     };
   };
 
+  services.open-webui = {
+    enable = true;
+    host = "0.0.0.0";
+    environment = {
+      OLLAMA_API_BASE_URL = "http://hulk.ereslibre.net:11434";
+      WEBUI_AUTH = "False";
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+    };
+  };
+
   services.matrix-synapse = {
     extraConfigFiles = [
       config.sops.secrets."matrix-synapse-registration-shared-secret.yaml".path
