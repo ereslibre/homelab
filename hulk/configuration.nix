f9d2fc176bc015fb6a3504f96350fb353720b42b
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-18 10:51:53 +0100

    Update

--- hulk/configuration.nix
+++ hulk/configuration.nix
@@ -33,7 +33,7 @@
   services.ollama = {
     enable = true;
     host = "0.0.0.0";
-    loadModels = ["llama3.3:70b-instruct-q2_K" "qwen2.5-coder:32b"];
+    loadModels = ["mistral:7b-instruct" "qwen2.5-coder:32b"];
   };
 
   # This value determines the NixOS release from which the default
