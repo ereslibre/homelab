6bebf226837d5bc6357a240a9f9f8d5b0457f2f3
Rafael Fernández López <ereslibre@ereslibre.es>
2025-01-12 18:04:38 +0100

    Update

--- common/nixos/default.nix
+++ common/nixos/default.nix
@@ -5,6 +5,10 @@
       automatic = true;
       options = "--delete-older-than 30d";
     };
-    settings.trusted-users = ["root" "builder" "ereslibre"];
+    settings = {
+      substituters = ["https://cache.garnix.io"];
+      trusted-users = ["root" "builder" "ereslibre"];
+      trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+    };
   };
 }
