02e0b8378ddf45e0cf57dece19f7ceb70a940ee4
Rafael Fernández López <ereslibre@ereslibre.es>
2025-05-18 10:29:02 +0200

    Update

--- .switch.sh
+++ .switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$(uname -o)" == "Darwin" ]]; then
-    nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin -- switch --flake .#"$@"
+    sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin -- switch --flake .#"$@"
 else
     sudo nixos-rebuild --flake .#"$@" switch
 fi
