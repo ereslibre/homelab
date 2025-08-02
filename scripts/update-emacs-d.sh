3cc7499e98198957da2e8e035413e28c53ff61e7
Rafael Fernández López <ereslibre@ereslibre.es>
2025-08-03 00:18:45 +0200

    Update

--- scripts/update-emacs-d.sh
+++ scripts/update-emacs-d.sh
@@ -10,7 +10,7 @@ mkdir -p dotfiles/assets/emacs/emacs.d/elpa
 just
 ~/.nix-profile/bin/emacs --batch --debug-init --load "${SCRIPT_DIR}/extra/repos.el" --load ~/.emacs.d/early-init.el --load ~/.emacs.d/init.el --load ~/.emacs.d/custom.el -f package-refresh-contents
 ~/.nix-profile/bin/emacs --batch --debug-init --load "${SCRIPT_DIR}/extra/repos.el" --load ~/.emacs.d/early-init.el --load ~/.emacs.d/init.el --load ~/.emacs.d/custom.el -f kill-emacs
-cp -r ~/.emacs.d/elpa dotfiles/assets/emacs/emacs.d
+cp -rL ~/.emacs.d/elpa dotfiles/assets/emacs/emacs.d
 rm -rf ~/.emacs.d/elpa
 git add dotfiles/assets/emacs
 just
