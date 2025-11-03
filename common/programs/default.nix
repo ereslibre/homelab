533cd1ed616517de6583f8f7f37fe3e6ec5be46a
Rafael Fernández López <ereslibre@ereslibre.es>
2025-11-03 14:01:19 +0100

    chore: add missing libraries to `nix-ld`
    This still fails to run on a remote machine due to a missing X display

--- common/programs/default.nix
+++ common/programs/default.nix
@@ -1,6 +1,33 @@
-{
+{pkgs, ...}: {
   programs = {
-    nix-ld.enable = true;
+    nix-ld = {
+      enable = true;
+
+      # [rover] Fixes an issue when running tests on suites that download vscode binaries: [test:extension] /home/ereslibre/projects/endorhq/rover/packages/extension/.vscode-test/vscode-linux-x64-1.105.1/code: error while loading shared libraries: libglib-2.0.so.0: cannot open shared object file: No such file or directory
+      libraries = with pkgs; [
+        alsa-lib
+        at-spi2-atk
+        cairo
+        dbus.lib
+        expat
+        glib
+        gtk3
+        libgbm
+        libxkbcommon
+        nspr
+        nss_latest
+        pango
+        udev.dev
+        xorg.libX11
+        xorg.libxcb
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+      ];
+    };
+
     zsh.enable = true;
   };
 }
