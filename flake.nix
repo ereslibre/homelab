e1b6a13abeaea9454b33fefe02638e7be3c23a17
Rafael Fernández López <ereslibre@ereslibre.es>
2026-01-11 21:09:39 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "Home lab";
 
+  nixConfig = {
+    extra-substituters = ["https://cache.numtide.com"];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
   inputs = {
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
