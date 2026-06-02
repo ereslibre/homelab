e6b4170acb02eae18d25b3f27be3020b2489a490
Rafael Fernández López <ereslibre@ereslibre.es>
2026-06-02 17:41:20 +0200

    Update

--- common/packages/default.nix
+++ common/packages/default.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    lm_sensors
-    ltrace
-    man-pages
-    man-pages-posix
-  ];
+{
+  pkgs,
+  nix-ai-tools,
+  ...
+}: let
+  ai-tools = nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system};
+in {
+  environment.systemPackages =
+    (with pkgs; [
+      lm_sensors
+      ltrace
+      man-pages
+      man-pages-posix
+    ])
+    ++ (with ai-tools; [ccusage]);
 }
